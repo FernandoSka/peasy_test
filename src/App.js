f87bc2f8d385d9c9f750364389212fb559c8841b
FernandoSka <yayo280.fl@gmail.com>
2024-03-18 22:25:20 -0600

    se crea carga de archivos csv

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import './service/example.js'
+import Papa from 'papaparse';
 import React, { useState, useEffect } from 'react';
+import ReactFileReader from "react-file-reader";
 import PersonService from './service/PersonService'
 import PersonDataProxy from './service/PersonDataProxy'
 
@@ -14,7 +16,41 @@ function App() {
   const [names_load, setNamesLoad] = useState(false);
   const [name_create, setNameCreate] = useState("");
   
+  const uploadFile = (files) => {
+    // Creating the object of FileReader Class
+    var read = new FileReader();
 
+  // when readAsText will invoke, onload() method on the read object will execute.
+    Papa.parse(files[0], {
+      complete: function(results) {
+          // results contain data; users can use the data for some operations.
+            console.log("Finished:", results.data);
+            var name = results.data[0][0];
+            var last_name = results.data[0][1];
+            var items = [];
+            results.data.slice(1).forEach(element => {
+              items.push({
+                name: element[0],
+                last_name: element[1]
+              })
+            });
+            items.forEach(element => {
+              var command = personService.insertCommand({name: element["name"] +" " + element["last_name"], field: "nombre"});
+
+              command.execute().then(result => {
+                if (result.success) {
+                  console.log(result.value); // prints the inserted object with the assigned id
+                  set_names_load()
+                }
+                else {
+                  console.log(result.errors); // prints {association: "name", message: "Name cannot be Jimi Hendrix"}
+                }
+              });
+            });
+
+      }
+    });
+  };
 
   useEffect(() => {
     const get_names = ()=>{
@@ -48,6 +84,8 @@ function App() {
       }
     });
 
+    
+
   }
   return (
     <div className="App">
@@ -78,6 +116,11 @@ function App() {
           <input onChange={event=> {setNameCreate(event.target.value)}} />
           <button type="submit">Submit</button>
         </form>
+        <h3> Upload a CSV file to read</h3>
+        {/* creating the file upload button to upload CSV file */}
+        <ReactFileReader handleFiles = {uploadFile} fileTypes={".csv"}>
+          <button className="btn"> Upload </button>
+        </ReactFileReader>
       </header>
     </div>
   );
